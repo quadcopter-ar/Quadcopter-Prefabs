7b4601750d20461953b9d08c647a38a4c6a4d291
Vybhav Achar Bhargav <contact@vybhavb.com>
2021-03-30 14:27:42 -0700

    initial commit with all controller files setup

---
+++ Assets/Scripts/Input/Primary2DAxis/Primary2DAxisWatcher.cs
@@ -0,0 +1,11 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.XR;
+
+[System.Serializeable]
+public class Primary2DAxisEvent : UnityEvent<bool> { }
+
+public class Primary2DAxisWatcher : MonoBehavior {
+   
+}
