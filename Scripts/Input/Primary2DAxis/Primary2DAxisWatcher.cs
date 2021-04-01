70b5480992cdd2b8853c0212e023e10fd159f4b2
Vybhav Achar Bhargav <contact@vybhavb.com>
2021-03-31 17:26:02 -0700

    Moving to a package system for the base system

--- Scripts/Input/Primary2DAxis/Primary2DAxisWatcher.cs
+++ Scripts/Input/Primary2DAxis/Primary2DAxisWatcher.cs
@@ -1,11 +1,7 @@
 using System.Collections.Generic;
+using System.Collections;
 using UnityEngine;
-using UnityEngine.Events;
-using UnityEngine.XR;
 
-[System.Serializeable]
-public class Primary2DAxisEvent : UnityEvent<bool> { }
+public class Primary2DAxisWatcher : MonoBehaviour {
 
-public class Primary2DAxisWatcher : MonoBehavior {
-   
 }
