f0d9fe705b4ab71e8ad7dc64fabb05ee2448291a
Vybhav Achar Bhargav <contact@vybhavb.com>
2021-05-12 23:49:58 -0700

    adding xbox controller input support.

--- Assets/Scripts/Input/BasicMovement.cs
+++ Assets/Scripts/Input/BasicMovement.cs
@@ -2,50 +2,181 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.XR;
+using UnityEngine.InputSystem;
 
 public class BasicMovement : MonoBehaviour
 {
     float deadZoneAmount = 0.5f;
+    public float speed = 10;
 
+    public bool debugMode = false;
     private XRNode leftControllerNode = XRNode.LeftHand;
-    private List<InputDevice> leftInputDevices = new List<InputDevice>();
+    private List<UnityEngine.XR.InputDevice> leftInputDevices = new List<UnityEngine.XR.InputDevice>();
 
-    private InputDevice leftController;
-    private XRNode rightControllerNode = XRNode.LeftHand;
-    private List<InputDevice> rightInputDevices = new List<InputDevice>();
-    private InputDevice rightController;
+    private UnityEngine.XR.InputDevice leftController;
+    private XRNode rightControllerNode = XRNode.RightHand;
+    private List<UnityEngine.XR.InputDevice> rightInputDevices = new List<UnityEngine.XR.InputDevice>();
+    private UnityEngine.XR.InputDevice rightController;
+
+    private Gamepad gamepad;
+
 
     void Start() {
         //Lets get all the devices we can find.
-        GetDevices();
+        if(!debugMode){
+          GetDevices();
+        }else{
+          gamepad = Gamepad.current;
+          if(gamepad == null){
+            Debug.LogError("No gamepad connected");
+            return;
+          }
+        }
     }
 
     void Update() {
-        if (leftController == null) {
-            GetControllerDevices(leftControllerNode, leftController, leftInputDevices);
-        }
+        if(!debugMode){
+            if (leftController == null) {
+                GetControllerDevices(leftControllerNode, ref leftController, ref leftInputDevices);
+            }
 
-        if (rightController == null) {
-            GetControllerDevices(rightControllerNode, rightController, rightInputDevices);
-        }
+            if (rightController == null) {
+                GetControllerDevices(rightControllerNode, ref rightController, ref rightInputDevices);
+            }
 
-        CheckForChanges();
+            CheckForChanges();
+        }else{
+          Vector2 leftStick = gamepad.leftStick.ReadValue();
+          Vector2 rightStick = gamepad.rightStick.ReadValue();
+
+          if(leftStick != Vector2.zero){
+            if(leftStick.x < -deadZoneAmount){
+              MoveRight(leftStick.x);
+            }else if(leftStick.x > deadZoneAmount){
+              MoveLeft(leftStick.x);
+            }
+
+            if (leftStick.y < -deadZoneAmount) {
+                // touching bottom side, move backwards
+                MoveForward(leftStick.y);
+            } else if (leftStick.y > deadZoneAmount) {
+                // touching top side, move forward
+                MoveBackward(leftStick.y);
+            }
+          }
+
+
+          if(rightStick != Vector2.zero){
+            if(rightStick.x < -deadZoneAmount){
+              RotateLeft(rightStick.x);
+            }else if(rightStick.x > deadZoneAmount){
+              RotateRight(rightStick.x);
+            }
+
+            if (rightStick.y < -deadZoneAmount) {
+              MoveUp(rightStick.y);
+            } else if (rightStick.y > deadZoneAmount) {
+                // touching top side, move forward
+                MoveDown(rightStick.y);
+            }
+          }
+
+        }
     }
 
+    //Here we need to add code to work
     void CheckForChanges() {
+        Vector2 leftTouchCoords;
+        Vector2 rightTouchCoords;
+        bool triggerVal;
 
+        if (leftController.TryGetFeatureValue(UnityEngine.XR.CommonUsages.primary2DAxis, out leftTouchCoords) && leftTouchCoords != Vector2.zero)
+        {
+
+            if(leftTouchCoords.x < -deadZoneAmount){
+              MoveLeft(leftTouchCoords.x);
+            }else if(leftTouchCoords.x > deadZoneAmount){
+              MoveRight(leftTouchCoords.x);
+            }
+
+            if (leftTouchCoords.y < -deadZoneAmount) {
+                MoveBackward(leftTouchCoords.y);
+            } else if (leftTouchCoords.y > deadZoneAmount) {
+                MoveForward(leftTouchCoords.y);
+            }
+        }
+
+        if (rightController.TryGetFeatureValue(UnityEngine.XR.CommonUsages.primary2DAxis, out rightTouchCoords) && rightTouchCoords != Vector2.zero)
+        {
+            if(rightTouchCoords.x < -deadZoneAmount){
+              RotateLeft(rightTouchCoords.x);
+            }else if(rightTouchCoords.x > deadZoneAmount){
+              RotateRight(rightTouchCoords.x);
+            }
+
+            if (rightTouchCoords.y < -deadZoneAmount) {
+              MoveDown(rightTouchCoords.y);
+            } else if (rightTouchCoords.y > deadZoneAmount) {
+              MoveUp(rightTouchCoords.y);
+            }
+        }
+
+    }
+
+    void MoveForward(float input){
+      MoveObject(Vector3.forward * input * Time.deltaTime * speed);
+    }
+    void MoveBackward(float input){
+      MoveObject(Vector3.back * input * Time.deltaTime * -speed);
+    }
+
+    void MoveLeft(float input){
+      MoveObject(Vector3.left* input * Time.deltaTime * -speed);
+    }
+
+    void MoveRight(float input){
+      MoveObject(Vector3.right * input * Time.deltaTime * speed);
+    }
+
+    void MoveUp(float input){
+      MoveObject(Vector3.up * input * Time.deltaTime * speed);
+    }
+
+    void MoveDown(float input){
+      MoveObject(Vector3.down * input * Time.deltaTime * -speed);
+    }
+
+    void RotateLeft(float input){
+      RotateObject(Vector3.up * input * Time.deltaTime * speed*10);
+    }
+
+    void RotateRight(float input){
+      transform.Rotate(Vector3.down * input * Time.deltaTime * -speed * 10);
+    }
+    void MoveObject(Vector3 tranlation){
+      transform.Translate(tranlation);
+    }
+
+    void RotateObject(Vector3 rotation){
+      transform.Rotate(rotation);
     }
 
     void GetDevices() {
         //Gets the Right Controller Devices
-        GetControllerDevices(leftControllerNode, leftController, leftInputDevices);
+        GetControllerDevices(leftControllerNode, ref leftController, ref leftInputDevices);
 
         //Gets the Right Controller Devices
-        GetControllerDevices(rightControllerNode, rightController, rightInputDevices);
+        GetControllerDevices(rightControllerNode, ref rightController, ref rightInputDevices);
+
+
+        Debug.Log(string.Format("Device name '{0}' with characteristics '{1}'", leftController.name, leftController.characteristics));
+
+        Debug.Log(string.Format("Device name '{0}' with characteristics '{1}'", rightController.name, rightController.characteristics));
 
     }
 
-    void GetControllerDevices(XRNode controllerNode, InputDevice controller,List<InputDevice> inputDevices) {
+    void GetControllerDevices(XRNode controllerNode, ref UnityEngine.XR.InputDevice controller,ref List<UnityEngine.XR.InputDevice> inputDevices) {
+        Debug.Log("Get devices is called");
         UnityEngine.XR.InputDevices.GetDevicesAtXRNode(controllerNode, inputDevices);
 
         if (inputDevices.Count == 1){
--- Assets/Scripts/Input/Primary2DAxis/Primary2DAxisWatcher.cs
+++
@@ -1,7 +0,0 @@
-using System.Collections.Generic;
-using System.Collections;
-using UnityEngine;
-
-public class Primary2DAxisWatcher : MonoBehaviour {
-
-}
